ec55f41195c8a05564b3d211b8b54e2c1afe27c6
asalander <59830304+asalander@users.noreply.github.com>
2020-02-13 00:37:14 +0200

    change

--- WS0/W09.js
+++ WS0/W09.js
@@ -1,13 +1,14 @@
-var http=require("http");
-http.createServer(function (request, response){
-
+var http = require("http");
+http
+  .createServer(function(request, response) {
     //SEnd the HTTP header. HTTP STatus: 200=OK
     //Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h1>Big Big World!</h1> ');
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write("<h1>Big Big World!</h1> ");
     //Send the response body as "Hello World"
-    response.end('<h2>Hello World!</h2>\n');
-}).listen(8081);
+    response.end("<h2>Hello World!!</h2>\n");
+  })
+  .listen(8081);
 
 //Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");

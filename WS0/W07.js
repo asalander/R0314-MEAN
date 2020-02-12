2160d97d789139ac2b7c26019e87f398e467e64f
asalander <59830304+asalander@users.noreply.github.com>
2020-02-13 00:28:28 +0200

    change line 8

--- WS0/W07.js
+++ WS0/W07.js
@@ -1,11 +1,13 @@
-var http=require("http");
-http.createServer(function (request, response){
+var http = require("http");
+http
+  .createServer(function(request, response) {
     //eEnd the HTTP header. HTTP STatus: 200=OK
     //Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.writeHead(200, { "Content-Type": "text/plain" });
     //Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(8081);
+    response.end("Hello World!\n");
+  })
+  .listen(8081);
 
 //Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");

c96f4c848afd04bfb2d709993816919afca91baa
asalander <59830304+asalander@users.noreply.github.com>
2020-02-13 00:24:15 +0200

    added

---
+++ WS0/W09.js
@@ -0,0 +1,13 @@
+var http=require("http");
+http.createServer(function (request, response){
+
+    //SEnd the HTTP header. HTTP STatus: 200=OK
+    //Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h1>Big Big World!</h1> ');
+    //Send the response body as "Hello World"
+    response.end('<h2>Hello World!</h2>\n');
+}).listen(8081);
+
+//Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
